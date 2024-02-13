ef388ba1ea7797bfc57e26964caeb97042f296cb
nelsonokothguya <nelsonokothguya15@gmail.com>
2024-02-13 21:25:06 +0300

    error handling, notfound, trycatch

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -21,7 +21,11 @@ export async function createInvoice(formData : FormData) {
    const amountInCents = amount * 100;
    const date = new Date().toISOString().split('T')[0];
 
-   await sql`INSERT INTO invoices (customer_id, amount, status, date) VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
+  try {
+    await sql`INSERT INTO invoices (customer_id, amount, status, date) VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
+  } catch (error: any) {
+    return {error: error.message}
+  }
 
     revalidatePath('/dashboard/invoices')
     redirect('/dashboard/invoices')
@@ -36,15 +40,25 @@ export async function updateInvoice(id: string, formData : FormData) {
 
     const amountInCents = amount * 100;
 
-    await sql`UPDATE invoices SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status} WHERE id = ${id}`;
+    try {
+        await sql`UPDATE invoices SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status} WHERE id = ${id}`;
+    } catch (error: any) {
+        return {error: error.message}
+    }
 
     revalidatePath('/dashboard/invoices')
     redirect('/dashboard/invoices')
 }
 
 export async function deleteInvoice(id: string) {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    // throw new Error('Not implemented')
+    try {
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+        revalidatePath('/dashboard/invoices') //only if try succeeds
+    } catch (error: any) {
+        return {error: error.message}
+    }
 
-    revalidatePath('/dashboard/invoices')
+    
     
 }
