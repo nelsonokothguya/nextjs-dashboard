f2e016ce8cdc34cf278b5bf14f87de09dc816bea
nelsonokothguya <nelsonokothguya15@gmail.com>
2024-03-20 11:54:25 +0300

    new customer components

--- app/ui/customers/create-form.tsx
+++ app/ui/customers/create-form.tsx
@@ -2,3 +2,12 @@ import { useFormState } from 'react-dom';
 import { Customer } from '../../lib/definitions';
 import Link from 'next/link';
 import { Button } from '../button';
+
+export default function Form({ customers }: { customers: Customer[] }) {
+  return (
+    <div>
+      <p>Provide Customer Details</p>
+      <Button>Submit</Button>
+    </div>
+  );
+}
